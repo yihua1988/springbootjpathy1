29812a5dfb4215e7087d7dd6d2d7e036b2ae156d
yihua <2505818092@qq.com>
2020-05-18 20:43:23 +0800

    test

--- src/main/java/com/offcn/springbootjpathy1/Springbootjpathy1Application.java
+++ src/main/java/com/offcn/springbootjpathy1/Springbootjpathy1Application.java
@@ -11,6 +11,7 @@ public class Springbootjpathy1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Springbootjpathy1Application.class, args);
+        System.out.println("我是1.0");
     }
 
 }
