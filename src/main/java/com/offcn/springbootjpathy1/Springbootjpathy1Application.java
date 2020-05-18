49c06c8371511c0e99b97628e52e8f59f19f2801
yihua <2505818092@qq.com>
2020-05-18 19:08:53 +0800

    test

--- src/main/java/com/offcn/springbootjpathy1/Springbootjpathy1Application.java
+++ src/main/java/com/offcn/springbootjpathy1/Springbootjpathy1Application.java
@@ -3,6 +3,9 @@ package com.offcn.springbootjpathy1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class Springbootjpathy1Application {
 
